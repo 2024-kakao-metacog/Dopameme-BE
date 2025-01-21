0db15652c3c8cd15eac7057969707856ae2c1afb
Shin <skynet3285@gmail.com>
2025-01-21 11:58:02 +0900

    fix: cicd build no cache

--- .github/scripts/build_and_push.sh
+++ .github/scripts/build_and_push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Building Docker image using docker-compose..."
-docker-compose build
+# docker-compose build
+docker-compose build --no-cache
 
 echo "Pushing Docker image to Docker Hub..."
 docker-compose push
