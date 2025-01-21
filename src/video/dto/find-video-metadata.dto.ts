d834014247f28b2b839209aa3cf0091c7a27b93b
Shin <skynet3285@gmail.com>
2025-01-21 11:37:10 +0900

    feat: video metadata

--- src/video/dto/find-video-metadata.dto.ts
+++ src/video/dto/find-video-metadata.dto.ts
@@ -1,8 +1,14 @@
-import { IsInt, Max, Min } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
 
 export class FindVideoMetadataListDto {
   @IsInt()
   @Min(1, { message: 'Minimum value is 1' })
   @Max(20, { message: 'Maximum value is 20' })
   maxResults: number;
+}
+
+export class FindVideoMetadataDto {
+  @IsString()
+  @IsNotEmpty()
+  videoUrl: string;
 }
