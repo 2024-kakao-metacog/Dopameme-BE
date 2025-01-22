4bfbf13d419d87b2b820c73d207c97ce050ea813
yunsammy <yun3.jo@gmail.com>
2025-01-22 13:22:33 +0900

    Feat: create jwt auth guard using passport strategy

--- src/auth/strategy/jwt-auth.strategy.ts
+++ src/auth/strategy/jwt-auth.strategy.ts
@@ -1,35 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AccessTokenPayload } from '../entities/access-token-payload.entity';
 import { JwtConfig } from '../../config/jwt.config';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class JwtAccessTokenStrategy extends PassportStrategy(
+export class JwtAuthStrategy extends PassportStrategy(
   Strategy,
   'access_token',
 ) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
     const jwtConfig = configService.get<JwtConfig>('jwt');
 
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request) => {
-          console.log(request.cookies);
-          return request?.cookies?.access_token;
-        },
-      ]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: jwtConfig.publicKey,
+      algorithms: ['RS256'],
       ignoreExpiration: false,
       passReqToCallback: true,
     });
   }
 
-  validate(req: Request, payload: AccessTokenPayload) {
-    console.log('Access Token Strategy:', payload);
-    req.user = payload;
-    return payload;
+  async validate(request: string, payload: AccessTokenPayload) {
+    const user = await this.userService.findUserByUserId(payload.userId);
+
+    if (user === null) {
+      return null;
+    }
+
+    return user;
   }
 }
