b826557cad5cb026a0413034f1fc944ba6101a14
yunsammy <yun3.jo@gmail.com>
2025-01-22 13:23:28 +0900

    Update: export jwt auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,13 @@ import { AuthService } from './auth.service';
 import { DatabaseService } from '../database.service';
 import { UserService } from '../user/user.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthStrategy } from './strategy/jwt-auth.strategy';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
 
 @Module({
   imports: [JwtModule.register({ global: true })],
   controllers: [AuthController],
-  providers: [AuthService, UserService, DatabaseService],
+  providers: [AuthService, UserService, DatabaseService, JwtAuthStrategy],
+  exports: [JwtAuthGuard],
 })
 export class AuthModule {}
