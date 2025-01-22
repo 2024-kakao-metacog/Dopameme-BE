1777511b45bbc14d91f84678e78880c476881c2d
yunsammy <yun3.jo@gmail.com>
2025-01-22 13:22:33 +0900

    Feat: create jwt auth guard using passport strategy

---
+++ src/auth/guard/jwt-auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('access_token') {
+  handleRequest<TUser = any>(err: any, user: TUser, info: any): TUser {
+    // TokenExpiredError 발생 시 처리
+    if (info?.name === 'TokenExpiredError') {
+      throw new UnauthorizedException('Your access token has expired');
+    }
+
+    if (err || !user) {
+      throw err || new UnauthorizedException('Unauthorized access');
+    }
+
+    return user;
+  }
+}
