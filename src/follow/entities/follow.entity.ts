93a41d073659829a91f8c4db5bc60f9c708e8446
Shin <skynet3285@gmail.com>
2025-01-23 15:44:45 +0900

    refactor: user-related schema for follow

--- src/follow/entities/follow.entity.ts
+++ src/follow/entities/follow.entity.ts
@@ -1,6 +1,6 @@
 export class Follow {
   id: number;
-  followingUserId: number; // from
-  followedUserId: number; // to
+  followedUserId: string;
+  followedNickname: string;
   createdAt: Date;
 }
