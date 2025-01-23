0e03ed2ae457936eec6aa85dec79d30660e08a7d
Shin <skynet3285@gmail.com>
2025-01-23 15:44:45 +0900

    refactor: user-related schema for follow

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -3,10 +3,11 @@ import { FollowService } from './follow.service';
 import { FollowController } from './follow.controller';
 import { DatabaseService } from '../database.service';
 import { AuthModule } from '../auth/auth.module';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [FollowController],
-  providers: [FollowService, DatabaseService],
+  providers: [FollowService, DatabaseService, UserService],
 })
 export class FollowModule {}
--- src/follow/follow.service.ts
+++ src/follow/follow.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../database.service';
-import { Follow } from '@prisma/client';
+import { UserService } from '../user/user.service';
+import { Follow } from './entities/follow.entity';
 
 @Injectable()
 export class FollowService {
-  constructor(private readonly prisma: DatabaseService) {}
+  constructor(
+    private readonly prisma: DatabaseService,
+    private readonly userService: UserService,
+  ) {}
 
-  async createFollow(fromUserId: number, toUserId: number): Promise<Follow> {
+  async createFollow(
+    fromUserId_idx: number,
+    toUserId: string,
+  ): Promise<Follow> {
+    const toUser = await this.userService.findUserByUserId(toUserId);
+
+    if (toUser === null) {
+      throw new Error(`User not found`);
+    }
+
     const follow = await this.prisma.follow.findFirst({
       where: {
-        followingUserId: fromUserId,
-        followedUserId: toUserId,
+        followingUserId: fromUserId_idx,
+        followedUserId: toUser.id,
       },
     });
 
@@ -20,18 +33,61 @@ export class FollowService {
 
     const newFollow = await this.prisma.follow.create({
       data: {
-        followingUserId: fromUserId,
-        followedUserId: toUserId,
+        followingUserId: fromUserId_idx,
+        followedUserId: toUser.id,
         createdAt: new Date(),
       },
     });
-    return newFollow;
+    return {
+      id: newFollow.id,
+      followedUserId: toUser.userId,
+      followedNickname: toUser.nickname,
+      createdAt: newFollow.createdAt,
+    };
   }
 
   async findFollowingsByUserId(fromUserId: number): Promise<Follow[]> {
-    return await this.prisma.follow.findMany({
+    const follows = await this.prisma.follow.findMany({
       where: {
         followingUserId: fromUserId,
+      },
+    });
+
+    return await Promise.all(
+      follows.map(async (follow) => {
+        const followed = await this.userService.findUserById(
+          follow.followedUserId,
+        );
+        return {
+          id: follow.id,
+          followedUserId: followed.userId,
+          followedNickname: followed.nickname,
+          createdAt: follow.createdAt,
+        };
+      }),
+    );
+  }
+
+  async removeFollowingByUserIdAndId(
+    userId: number,
+    followId: number,
+  ): Promise<void> {
+    const follow = await this.prisma.follow.findUnique({
+      where: {
+        id: followId,
+      },
+    });
+
+    if (follow === null) {
+      throw new Error(`Not following user`);
+    }
+    if (follow.followingUserId !== userId) {
+      throw new Error(`Unauthorized`);
+    }
+
+    await this.prisma.follow.delete({
+      where: {
+        id: followId,
       },
     });
   }
