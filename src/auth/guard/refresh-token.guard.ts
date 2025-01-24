378579f822c142ffa65ac929ed7300a30aa0c0c2
yunsammy <yun3.jo@gmail.com>
2025-01-24 22:40:27 +0900

    Rename: rename JwtAuthGuard & JwtRefreshGuard

--- src/auth/guard/refresh-token.guard.ts
+++ src/auth/guard/refresh-token.guard.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtRefreshGuard extends AuthGuard('refresh_token') {
+export class RefreshTokenGuard extends AuthGuard('refresh_token') {
   handleRequest<TUser = any>(err: any, user: TUser, info: any): TUser {
     // TokenExpiredError 발생 시 처리
     if (info?.name === 'TokenExpiredError') {
