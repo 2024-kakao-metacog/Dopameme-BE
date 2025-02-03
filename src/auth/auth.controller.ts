eabafd49de3ef97a5daf5ab83cdf76ed943e2e39
yunsammy <yun3.jo@gmail.com>
2025-02-03 10:35:49 +0900

    Feat: add guard for validate & regenerate refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,6 @@ import {
   Put,
   Res,
   UnauthorizedException,
-  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {
@@ -21,7 +20,6 @@ import {
 import { LoginUserDto } from '../user/dto/login-user.dto';
 import { Cookies } from './decorator/cookies.decorator';
 import { Response } from 'express';
-import { JwtRefreshGuard } from './guard/jwt-refresh.guard';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -65,7 +63,6 @@ export class AuthController {
   }
 
   @Put()
-  @UseGuards(JwtRefreshGuard)
   @ApiOperation({ summary: 'Refresh Authentication Token' })
   @HttpCode(HttpStatus.OK)
   @ApiBody({
--- src/auth/strategy/jwt-refresh.strategy.ts
+++ src/auth/strategy/jwt-refresh.strategy.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { RefreshTokenPayload } from '../entities/refresh-token-payload.entity';
 import { JwtConfig } from '../../config/jwt.config';
 import { UserService } from 'src/user/user.service';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
@@ -15,6 +16,7 @@ export class JwtRefreshStrategy extends PassportStrategy(
   constructor(
     private readonly configService: ConfigService,
     private readonly userService: UserService,
+    private readonly authService: AuthService,
   ) {
     const jwtConfig = configService.get<JwtConfig>('jwt');
 
@@ -36,6 +38,18 @@ export class JwtRefreshStrategy extends PassportStrategy(
     if (user === null) {
       return null;
     }
+
+    const res: Response = req.res;
+
+    const { accessToken, refreshToken } =
+      await this.authService.createAuthTokens(user);
+
+    res.setHeader('Authorization', `Bearer ${accessToken}`);
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: true,
+    });
 
     // TODO: payload 넘기는 것으로 수정 필요(security)
     return user;
